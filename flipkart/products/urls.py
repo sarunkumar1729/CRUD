dbf174fb3018aa2cd0c52ac2dc9351de67d2d288
sarunkumar <sarunkumarvv1993@gmail.com>
2024-11-12 09:34:59 +0530

    second commit

--- flipkart/products/urls.py
+++ flipkart/products/urls.py
@@ -2,7 +2,10 @@ from django.urls import path
 from .import views
 
 urlpatterns = [
-    path('add_product_page',views.add_product_page,name='add_product_page'),
+    # path('add_product_page',views.add_product_page,name='add_product_page'),
     path('add_product',views.add_product,name='add_product'),
-    path('product_list',views.product_list,name='product_list')
+    path('product_list',views.product_list,name='product_list'),
+    path('update_product_page/<int:i>',views.update_product_page,name='update_product_page'),
+    path('update_product',views.update_product,name='update_product'),
+    path('delete_product/<int:i>',views.delete_product,name='delete_product')
 ]
--- flipkart/products/views.py
+++ flipkart/products/views.py
@@ -1,17 +1,52 @@
 from django.shortcuts import render,redirect
 from .models import Products
 
-def add_product_page(request):
-      return render(request,'add_product.html')
+# def add_product_page(request):
+#       return render(request,'add_product.html')
+
+# def add_product(request):
+#       name = request.POST['name']
+#       price = request.POST['price']
+#       qty = request.POST['qty']
+#       product = Products(name=name,price=price,quantity=qty)
+#       product.save()
+#       return redirect('add_product_page')
+
 
 def add_product(request):
-      name = request.POST['name']
-      price = request.POST['price']
-      qty = request.POST['qty']
-      product = Products(name=name,price=price,quantity=qty)
-      product.save()
-      return redirect('add_product_page')
+      if request.method=='POST':
+            name = request.POST['name']
+            price = request.POST['price']
+            qty = request.POST['qty']
+            product = Products(name=name,price=price,quantity=qty)
+            product.save()
+            return redirect('add_product')  
+      else:
+            return render(request,'add_product.html')
+
+
       
 def product_list(request):
       products = Products.objects.all()
       return render(request,'product_list.html',{'products':products})
+
+def update_product_page(request,i):
+      global product
+      product = Products.objects.get(id=i)
+      return render(request,'update_product.html',{'product':product})
+
+def update_product(request):
+      name = request.POST['name']
+      price = request.POST['price']
+      qty = request.POST['qty']
+      product.name = name 
+      product.price = price 
+      product.quantity = qty 
+      product.save()
+      return redirect('product_list')
+      
+
+def delete_product(request,i):
+      product = Products.objects.get(id=i)
+      product.delete()
+      return redirect('product_list')
